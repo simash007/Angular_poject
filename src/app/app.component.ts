8896512bef421b5f9572d4e4632dc8f53e9d051b
simash007 <ashutoshon3011@gmail.com>
2024-01-18 11:47:20 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NewCustomerRegistrationComponent } from './new-customer-registration/new-customer-registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RouterLink,LoginPageComponent,NewCustomerRegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NewCustomerRegistrationComponent } from './new-customer-registration/new-customer-registration.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {path:'', component: LoginPageComponent},
+    {path:'newCustomerReg', component: NewCustomerRegistrationComponent},
+
+
+];
